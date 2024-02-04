a1f198f01576d7729bb6e31e0f1834f5bf246b43
iambasantarai <iambasantarai@gmail.com>
2024-02-04 21:09:41 +0545

    Create a progress indicator component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import ProgressIndicator from "./components/ProgressIndicator";
+
 export default function App() {
   return (
-    <div className="relative isolate pt-14">
-      <div className="py-24 sm:py-32 lg:pb-40">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8 text-center">
-          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            Something in the way, hmm-mmm
-            <br />
-            Something in the way, yeah, hmm-mmm
-          </h1>
+    <>
+      <div className="relative isolate pt-14">
+        <div className="py-24 sm:py-32 lg:pb-40">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8 text-center">
+            <ProgressIndicator />
+
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Something in the way, hmm-mmm
+              <br />
+              Something in the way, yeah, hmm-mmm
+            </h1>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
---
+++ src/components/ProgressIndicator.jsx
@@ -0,0 +1,17 @@
+import "../progress_indicator.css";
+
+export default function ProgressIndicator() {
+  return (
+    <div className="lds-grid">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
